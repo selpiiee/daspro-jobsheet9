5216e65a985ed324e40e0752c0d7be1203dc26a8
“selpiiee” <selvynurkhoffifa@gmail.com>
2025-11-19 21:45:10 +0700

    Sel - Array Average Score 21 - ifelse

--- arrayAverageScore21.java
+++ arrayAverageScore21.java
@@ -1,9 +1,11 @@
+
 import java.util.Scanner;
 
 public class arrayAverageScore21 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int[] score = new int[10];
         double total = 0, average;
 
@@ -12,7 +14,13 @@ public class arrayAverageScore21 {
             score[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < score.length; i ++) {
+        for (int i = 0; i < score.length; i++) {
+            if (score[i] > 70) {
+                System.out.println("Student " + i + " Passed!");
+            } else {
+                System.out.println("Student " + i + " Failed!");
+            }
+
             total += score[i];
         }
 

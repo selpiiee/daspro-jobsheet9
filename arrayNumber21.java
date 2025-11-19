429e58c3b62279eee6837cf50da882b9e7daad88
“selpiiee” <selvynurkhoffifa@gmail.com>
2025-11-19 20:46:57 +0700

    Sel - Array Number 21 - for

--- arrayNumber21.java
+++ arrayNumber21.java
@@ -12,10 +12,9 @@ public class arrayNumber21 {
         num[2] = 7;
         num[3] = 20;
 
-        System.out.println(num[0]);
-        System.out.println(num[1]);
-        System.out.println(num[2]);
-        System.out.println(num[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(num[i]);
+        }
 
         sc.close();
     }

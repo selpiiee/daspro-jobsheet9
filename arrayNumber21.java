b26d3c988b1082660a6f9a62ce552d3467321070
“selpiiee” <selvynurkhoffifa@gmail.com>
2025-11-20 11:45:47 +0700

    Sel - Array Number 21 - initializing

--- arrayNumber21.java
+++ arrayNumber21.java
@@ -6,11 +6,8 @@ public class arrayNumber21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] num = new int[4];
-        num[0] = 5;
-        num[1] = 12;
-        num[2] = 7;
-        num[3] = 20;
+        int[] num = {5, 12, 7, 20};
+       
 
         for (int i = 0; i <= 4; i++) {
             System.out.println(num[i]);
